b062dc51c9104ece22dc73148a911fa1dcbf1e0e
Gabriel <gachix00@gmail.com>
2019-03-11 20:30:59 -0600

    Se agrego control de usuario repetidos y de eliminacion de usuarios, ademas se mejoraron los sockets del cliente para que no requieran desconectarse cada vez que se envia un mensaje

--- Client.c
+++ Client.c
@@ -15,13 +15,13 @@ void error(const char *msg)//muestra los diversos mensajes de error que pueden o
 
 int main(int argc, char *argv[])
 {
+
     int idSocket, numeroPuerto, n;
-    struct sockaddr_in serv_addr;//aqui se guarda la direccion del server al que nos vamos a conectar
+    struct sockaddr_in dirServer;//aqui se guarda la direccion del server al que nos vamos a conectar
     struct hostent *server;//hostsent es una estructura de netdb.h
 
-    char buffer[256];//buffer del mensaje que se va a enviar
+    char mensaje[256];//mensaje del mensaje que se va a enviar
     if (argc < 3) {
-       fprintf(stderr,"usage %s hostname port\n", argv[0]);
        exit(0);
     }
     numeroPuerto = atoi(argv[2]);//se consigue el numero de puerto del input
@@ -33,45 +33,47 @@ int main(int argc, char *argv[])
         fprintf(stderr,"Error, server no existe\n");
         exit(0);
     }
-    bzero((char *) &serv_addr, sizeof(serv_addr));//mismo proposito que en el server
-    serv_addr.sin_family = AF_INET;
+    bzero((char *) &dirServer, sizeof(dirServer));//mismo proposito que en el server
+    dirServer.sin_family = AF_INET;
     bcopy((char *)server->h_addr,
-         (char *)&serv_addr.sin_addr.s_addr,
+         (char *)&dirServer.sin_addr.s_addr,
          server->h_length);
-    serv_addr.sin_port = htons(numeroPuerto);
-    if (connect(idSocket,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0){//se llamma a connect para realizar una coneccion con el server el segundo argumento es el server
+    dirServer.sin_port = htons(numeroPuerto);
+    if (connect(idSocket,(struct sockaddr *) &dirServer,sizeof(dirServer)) < 0){//se llamma a connect para realizar una coneccion con el server el segundo argumento es el server
         error("No se pudo conectar");
       }
     printf("Ingrese su Usuario: ");
-    bzero(buffer,256);//se reinicia el buffer antes de pedir el mensaje
-    fgets(buffer,255,stdin);//se lee el mensaje
+    bzero(mensaje,256);//se reinicia el mensaje antes de pedir el mensaje
+    fgets(mensaje,255,stdin);//se lee el mensaje
     char Mensaje[2] = "#";
-    strcat(Mensaje,buffer);
-    n = write(idSocket,Mensaje,strlen(Mensaje));//se le escribe al servidor el mensaje
+    strcat(Mensaje,mensaje);
+    n = send(idSocket,Mensaje,strlen(Mensaje),0);//se le escribe al servidor el mensaje
     if (n < 0)
          error("Error al escribir al socket");
-    bzero(buffer,256);
-    n = read(idSocket,buffer,255);//se recibe respuesta de server
+    bzero(mensaje,256);
+    n = recv(idSocket,mensaje,255,0);//se recibe respuesta de server
     if (n < 0)
          error("Error leyendo del socket");
-    printf("%s\n",buffer);//se imprime la respuesta
-    
+    printf("%s\n",mensaje);//se imprime la respuesta
+    if(strcmp(mensaje,"El usuario ya existe")==0){
+      exit(0);
+    }
     while(1){
-      idSocket = socket(AF_INET, SOCK_STREAM, 0);
-      if (connect(idSocket,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0){//se llamma a connect para realizar una coneccion con el server el segundo argumento es el server
-          error("No se pudo conectar");
-        }
-      printf("Ingrese su Mensaje: ");
-      bzero(buffer,256);//se reinicia el buffer antes de pedir el mensaje
-      fgets(buffer,255,stdin);//se lee el mensaje
-      n = write(idSocket,buffer,strlen(buffer));//se le escribe al servidor el mensaje
+      bzero(mensaje,256);//se reinicia el mensaje antes de pedir el mensaje
+      printf("Ingrese su Mensaje($S para salir): ");
+      fgets(mensaje,255,stdin);//se lee el mensaje
+      n = send(idSocket,mensaje,strlen(mensaje),0);//se le escribe al servidor el mensaje
       if (n < 0)
            error("Error al escribir al socket");
-      bzero(buffer,256);
-      n = read(idSocket,buffer,255);//se recibe respuesta de server
+
+      bzero(mensaje,256);
+      n = recv(idSocket,mensaje,255,0);//se recibe respuesta de server
       if (n < 0)
            error("Error leyendo del socket");
-      printf("%s\n",buffer);//se imprime la respuesta
+      printf("%s\n",mensaje);//se imprime la respuesta
+      if(strstr(mensaje,"Adios")!=NULL){
+        exit(0);
+      }
     }
     close(idSocket);
     return 0;
--- Server.c
+++ Server.c
@@ -12,14 +12,14 @@
 
 struct Cliente{
   char Usuario[50];
-  int idSocket;
+  unsigned short idSocket;
   struct Cliente * Siguiente;
 };
 static int *contadorClientes = 0;
 struct Cliente* Clientes;
 
-struct Cliente encontrarCliente(int idSocketCliente){//encuentra en que posicion de la lista esta el cliente basado en el id de su socket
-  printf("%i,%i\n",idSocketCliente,*(&Clientes->idSocket));
+struct Cliente encontrarCliente(unsigned short idSocketCliente){//encuentra en que posicion de la lista esta el cliente basado en el id de su socket
+  //printf("%i,%i\n",idSocketCliente,*(&Clientes->idSocket));
   struct Cliente clienteActual = *Clientes;
   int contador = 0;
   while(contador != *contadorClientes){
@@ -33,39 +33,132 @@ struct Cliente encontrarCliente(int idSocketCliente){//encuentra en que posicion
   return clienteActual;
 }
 
+int ClienteRepetido(char * Usuario){//Busca si el usuario ya existe en la lista de clientes, retorna 1 si ya existe 0 si no
+  int contador = 0;
+  struct Cliente clienteActual = *Clientes;
+  while(contador!= *contadorClientes){
+    if(strcmp(clienteActual.Usuario,Usuario)==0){
+      return 1;
+    }
+    contador++;
+    if((clienteActual.Siguiente) == NULL){
+      return 0;
+    }
+    clienteActual = *(clienteActual.Siguiente);
+  }
+  return 0;
+}
+
+int elimiarCliente(char * Usuario){
+  int contador = 0;
+  struct Cliente * clienteActual = Clientes;
+  struct Cliente * clienteAnterior = Clientes;
+  while(contador!= *contadorClientes){
+    if(strcmp(clienteActual->Usuario,Usuario)==0){
+       if(contador==0){
+         strcpy( Clientes->Usuario,"");
+         return 1;
+       }
+       else{
+         clienteAnterior->Siguiente = (clienteActual->Siguiente);
+       }
+    }
+    contador++;
+    clienteAnterior = clienteActual;
+    clienteActual = (clienteActual->Siguiente);
+
+  }
+  return 0;
+}
+int insertarNuevoCliente(struct Cliente ClInsertar){
+  if(*contadorClientes==0){
+    *Clientes = ClInsertar;
+    return 1;
+  }
+
+  int contador = 1;
+  struct Cliente * clienteActual = Clientes;
+
+  while(clienteActual->Siguiente != NULL){
+
+    clienteActual = (clienteActual->Siguiente);
+  }
+  printf("%s\n","Si");
+  clienteActual->Siguiente = malloc(sizeof(struct Cliente));
+  strcpy(clienteActual->Siguiente->Usuario,ClInsertar.Usuario);
+  clienteActual->Siguiente->idSocket = ClInsertar.idSocket;
+  clienteActual->Siguiente->Siguiente= NULL;
+  printf("%s\n","Si2");
+  return 0;
+}
+
+int imprimirClientes(){
+  struct Cliente * clienteActual = Clientes;
+
+  while(clienteActual->Siguiente != NULL){
+    printf("%s\n",clienteActual->Usuario);
+    clienteActual = (clienteActual->Siguiente);
+  }
+  printf("%s\n",clienteActual->Usuario);
+  return 0;
+}
+
 void error(const char *msg)//muestra los diversos mensajes de error y cierra el server
 {
     perror(msg);
     exit(1);
 }
 
-void doStuff(int newIdSocket){
-  char buffer[256];
+void loopConeccion(int newIdSocket,struct sockaddr_in dirCliente){
+  char respuesta[50];
+  char mensaje[256];
   int n;
-  bzero(buffer,256);//vacia el buffer donde se recibira el mensaje
-  n = read(newIdSocket,buffer,255);//lee el mensaje del usuario
+  bzero(mensaje,256);//vacia el mensaje donde se recibira el mensaje
+  n = recv(newIdSocket,mensaje,255,0);//lee el mensaje del usuario
   if (n < 0){
       error("Error leyendo inf de socket");
   }
-  if(buffer[0]=='#'){
-    struct Cliente nuevoCliente;
-    strcat(nuevoCliente.Usuario,buffer);
-    nuevoCliente.idSocket = newIdSocket;
-    *Clientes=nuevoCliente;
-    *contadorClientes+=1;
-    printf("Se registro el Cliente:%s",buffer);
+  printf("%s\n",mensaje);
+  if(strcmp(mensaje,"$S\n")==0){
+    printf("%s\n","Se entro a adios");
+    strcat(respuesta,"Adios");
+    printf("%s\n","adios1");
+    struct Cliente ClienteAEliminar = encontrarCliente(dirCliente.sin_port);
+    printf("%s\n","adios2");
+    elimiarCliente(ClienteAEliminar.Usuario);
+    printf("%s\n","adios3");
+    *contadorClientes-=1;
+
+  }
+  else if(mensaje[0]=='#'){
+    int resultado = ClienteRepetido(mensaje);
+    if(resultado==1){
+      printf("%s\n","Repetido");
+      strcat(respuesta,"El usuario ya existe");
+    }
+    else{
+      struct Cliente nuevoCliente;
+      strcat(nuevoCliente.Usuario,mensaje);
+      nuevoCliente.idSocket = dirCliente.sin_port;
+      //*Clientes=nuevoCliente;
+      insertarNuevoCliente(nuevoCliente);
+      //imprimirClientes();
+      *contadorClientes+=1;
+      printf("Se registro el Cliente:%s",mensaje);
+      strcat(respuesta,"Se recibio el msj");
+    }
   }
   else{
-    printf("%i",*contadorClientes);
-    struct Cliente posCliente = encontrarCliente(newIdSocket);
-    printf("%s:%s\n",posCliente.Usuario,buffer);
+    printf("%s\n","Se entro a print");
+    //printf("%i",*contadorClientes);
+    struct Cliente posCliente = encontrarCliente(dirCliente.sin_port);
+    printf("%s:%s\n",posCliente.Usuario,mensaje);
   }
 
-  n = write(newIdSocket,"Se recibio el msj",18);//se envia mensaje de exito al cliente
+  n = send(newIdSocket,respuesta,strlen(respuesta),0);//se envia mensaje de exito al cliente
   if (n < 0){
       error("Error enviando inf a socket");
   }
-  close(newIdSocket);
 
 }
 
@@ -74,11 +167,12 @@ int main(int argc, char *argv[])
      contadorClientes = mmap(NULL, sizeof (* contadorClientes), PROT_READ | PROT_WRITE,
                  MAP_SHARED | MAP_ANONYMOUS, -1, 0);
 
-     Clientes = (struct Cliente *)mmap(0,sizeof(struct Cliente), PROT_READ | PROT_WRITE, MAP_SHARED|MAP_ANONYMOUS, -1, 0);
+     Clientes = (struct Cliente *)mmap(NULL,sizeof(struct Cliente), PROT_READ | PROT_WRITE, MAP_SHARED|MAP_ANONYMOUS, -1, 0);
+
      int numeroPuerto,idSocket, newIdSocket,forkID;
-     socklen_t clientSize;
-     char buffer[256];//aqui se guarda el mensaje que se recibe del cliente
-     struct sockaddr_in serverAddres, cli_addr;//sockaddr_in es una strcutura de la netinite/in.h aqui se crean dos de ellas
+     socklen_t tamanioCliente;
+     char mensaje[256];//aqui se guarda el mensaje que se recibe del cliente
+     struct sockaddr_in dirServer, dirCliente;//sockaddr_in es una strcutura de la netinite/in.h aqui se crean dos de ellas
      int n;
      if (argc < 2) {
          fprintf(stderr,"No se dio un puerto\n");
@@ -89,21 +183,21 @@ int main(int argc, char *argv[])
        error("Error iniciando socket");
      }
 
-     bzero((char *) &serverAddres, sizeof(serverAddres));//cambia el valor de serverAddres a 0
+     bzero((char *) &dirServer, sizeof(dirServer));//cambia el valor de dirServer a 0
      numeroPuerto = atoi(argv[1]);//se toma el argumento dado de puerto y se pasa a int
-     serverAddres.sin_family = AF_INET;//se le asigna las propiedades necesaries a serv_addres
-     serverAddres.sin_addr.s_addr = INADDR_ANY;
-     serverAddres.sin_port = htons(numeroPuerto);
-     if (bind(idSocket, (struct sockaddr *) &serverAddres,sizeof(serverAddres)) < 0){//bind conecnta el socket
-         error("Erro conectando con cliente");
+     dirServer.sin_family = AF_INET;//se le asigna las propiedades necesaries a serv_addres
+     dirServer.sin_addr.s_addr = INADDR_ANY;
+     dirServer.sin_port = htons(numeroPuerto);
+     if (bind(idSocket, (struct sockaddr *) &dirServer,sizeof(dirServer)) < 0){//bind conecnta el socket
+         error("Error conectando con cliente");
      }
 
 
      listen(idSocket,5);//el sucket se frena y empieza a escuchar cualquier coneccion posible
-     clientSize = sizeof(cli_addr);
+     tamanioCliente = sizeof(dirCliente);
      while(1){
-     newIdSocket = accept(idSocket,(struct sockaddr *) &cli_addr,&clientSize);//bloquea el programa hasta que un cliente se conecte al server
-
+     newIdSocket = accept(idSocket,(struct sockaddr *) &dirCliente,&tamanioCliente);//bloquea el programa hasta que un cliente se conecte al server
+     printf("%hu\n",dirCliente.sin_port);
      if (newIdSocket < 0){
          error("Error aceptando nuevo socket");
      }
@@ -113,7 +207,10 @@ int main(int argc, char *argv[])
      }
      if(forkID==0){
        close(idSocket);
-       doStuff(newIdSocket);
+       while(1){
+         loopConeccion(newIdSocket,dirCliente);
+       }
+
        exit(0);
      }
      else{
