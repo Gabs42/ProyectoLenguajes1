6f57249bfbeb094581c087c8cfbee59b735386f3
drodriguezalas <drodriguezalas@gmail.com>
2019-03-19 20:38:57 -0600

    Added functionality to select which file to open and IP handling

--- IOHandler.c
+++ IOHandler.c
@@ -2,13 +2,25 @@
 #include <stdlib.h>
 #include "IOHandler.h"
 
-//Recives a string and writes it to a text file, "port-txt"
-//Returns 0 if the operation was successful
-//Returns 1 if the operation was unsuccessful
-int writePort(char *ptr_port)
+//TO-DO add documentation
+int writePort(char *ptr_port, char *file)
 {
+	char IP[10];
 	FILE *ptr_file;
-	ptr_file = fopen("port.txt", "w");
+	ptr_file = fopen(file, "r");
+
+	if(!ptr_file)
+	{
+		printf("Could not read file\n");
+		return 1;
+	}	
+
+	fgets(IP, 10, ptr_file);
+	fgets(IP, 10, ptr_file);
+
+	fclose(ptr_file);	
+
+	ptr_file = fopen(file, "w");
 
 	if(!ptr_file || !ptr_port)
 		return 1;
@@ -17,66 +29,134 @@ int writePort(char *ptr_port)
 		fprintf(ptr_file, "%c", *ptr_port);
 		ptr_port++;
 	}
+	fprintf(ptr_file, "\n");
+	
+	char *ptr_IP = IP;
+	while(*ptr_IP != '\0'){
+		fprintf(ptr_file, "%c", *ptr_IP);
+		ptr_IP++;	
+	}
 
 	fclose(ptr_file);
 	return 0;
 }
 
-//Recives an int to be written as a port
+//Recives an int to be written as a port        MUST CHANGE
 //Does a conversion to string and uses writePort()
-int writePortInt(int port)
+int writePortInt(int port, char *file)
 {
 	char strPort[10];
 	sprintf(strPort, "%d", port);
-	writePort(strPort);
+	writePort(strPort, file);
 }
 
-//Recieves a char[] of a given size, and assigns it a value taken from "port.txt"
-char *readPort(char *buf)
+//Recieves a char[] of a given size, and assigns it a value taken from "port.txt" MUST CHANGE
+char *readPort(char *buf, char *file)
 {
 	FILE *ptr_file;
-	ptr_file = fopen("port.txt", "r");
+	ptr_file = fopen(file, "r");
 
 	if(!ptr_file)
 	{
 		printf("Could not read file\n");
 		return NULL;
 	}	
-	
-	while(fgets(buf, 10, ptr_file) != NULL)
-	{
-		//printf("%s", buf);
-	}
+
+	fgets(buf, 10, ptr_file);
 
 	fclose(ptr_file);
 }
 
-//Converts the result of readPort to int and returns the value;
-int readPortInt()
+//Converts the result of readPort to int and returns the value; MUST CHANGE
+int readPortInt(char *file)
 {
 	char rPort[10]; 
-	readPort(rPort);
+	readPort(rPort, file);
 	return (int) strtol(rPort, (char **)NULL, 10);
+}
+
+//TO-DO documentation
+char *readIP(char *buf, char *file)
+{
+	FILE *ptr_file;
+	ptr_file = fopen(file, "r");
+
+	if(!ptr_file)
+	{
+		printf("Could not read file\n");
+		return NULL;
+	}	
+
+	fgets(buf, 10, ptr_file);
+	fgets(buf, 10, ptr_file);
+
+	fclose(ptr_file);
+}
+
+//TO-DO documentation
+int writeIP(char* ptr_IP, char* file)
+{
+	char port[10];
+	FILE *ptr_file;
+	ptr_file = fopen(file, "r");
+
+	if(!ptr_file)
+	{
+		printf("Could not read file\n");
+		return 1;
+	}	
+
+	fgets(port, 10, ptr_file);
+
+	fclose(ptr_file);	
+
+	ptr_file = fopen(file, "w");
+
+	if(!ptr_file || !ptr_IP)
+		return 1;
+
+	char *ptr_port = port;
+	while(*ptr_port != '\0'){
+		fprintf(ptr_file, "%c", *ptr_port);
+		ptr_port++;	
+	}
+
+	while(*ptr_IP != '\0'){
+		fprintf(ptr_file, "%c", *ptr_IP);
+		ptr_IP++;
+	}
+
+	fclose(ptr_file);
+	return 0;
 }
 
 int main()
 {
 /*
-	char *wPort = "30";
-	writePort(wPort);
+	char *wPort = "34";
+	writePort(wPort, "server.conf");
 */
 /*
-	int wPortInt = 32;
-	writePortInt(wPortInt);
+	int wPortInt = 52;
+	writePortInt(wPortInt, "server.conf");
 */
 /*	
 	char rPort[10]; 
-	readPort(rPort);
+	readPort(rPort, "server.conf");
 	printf("%s\n", rPort);
 */
 /*
-	int rPortInt = readPortInt();
+	int rPortInt = readPortInt("server.conf");
 	printf("%d\n", rPortInt);
+*/
+/*
+	char rIP[10];
+	readIP(rIP, "server.conf");
+	printf("%s\n", rIP);
+*/
+/*
+	char *wIP = "8.8.8.8";
+	writeIP(wIP, "server.conf");
 */
 	return 0;
 }
--- IOHandler.h
+++ IOHandler.h
@@ -1,9 +1,12 @@
 #ifndef IOHandler_H
 #define IOHandler_H
 
-int writePort(char*);
-int writePortInt(int);
-char *readPort(char*);
-int readPortInt();
+int writePort(char* ptr_port, char* file);
+int writePortInt(int port, char* file);
+int writeIP(char* ptr_IP, char* file);
+char *readPort(char* buffer, char* file);
+int readPortInt(char* file);
+char *readIP(char* buffer, char* file);
+
 
 #endif 
